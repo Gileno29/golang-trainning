28ea9c45a8878717ac64f09f74e2ba1730f066cc
Gileno Duarte <gileno.cr.duarte@gmail.com>
2024-01-25 20:13:11 -0300

    pointers coments

--- pointers/main.go
+++ pointers/main.go
@@ -7,11 +7,13 @@ func main() {
 	myString = "Green"
 
 	log.Println("myString is set to: ", myString)
+	//use & for reference memory address
 	changeUsingPointer(&myString)
 	log.Println("myString is set to: ", myString)
 
 }
 
+//use * to pointer its self to make a pointer
 func changeUsingPointer(s *string) {
 	log.Println("teh value of s is: ", s)
 	newValue := "Red"
