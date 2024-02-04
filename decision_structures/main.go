326cbef3e284d59bedde1e6aeac585df588bd7ab
Gileno Duarte <gileno.cr.duarte@gmail.com>
2024-02-04 15:35:28 -0300

    decision structures

--- decision_structures/main.go
+++ decision_structures/main.go
@@ -11,4 +11,24 @@ func main() {
 	} else {
 		log.Println("isTrue is false")
 	}
+
+	myNum := 100
+	isTrue = true
+
+	if myNum > 99 && isTrue == true {
+		log.Println("myNum is greater than 99 and isTrue is set to true")
+	}
+
+	myVar := "Cat"
+	switch myVar {
+	case "cat":
+		log.Println("cat is set to cat")
+
+	case "dog":
+
+		log.Println("cat is set to dog")
+
+	default:
+		log.Println("Cat is something else")
+	}
 }
