feef536d9f68b9f447aece9a2c210c773acb903c
Gileno Duarte <gileno.cr.duarte@gmail.com>
2024-02-18 11:03:14 -0300

    djson

---
+++ json/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	myJson := `
+	[
+		{
+			"first_name": "Clark",
+			"last_name": "kent",
+			"hair_color": "black",
+			"has_dog" : true
+		},
+		{
+			"first_name": "Bruce",
+			"last_name": "Wayne",
+			"hair_color": "black",
+			"has_dog" : false	
+		}
+
+	]`
+}
